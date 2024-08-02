f45d060e04da929d758244d0553e170a4754f894
Oliwier Juncewicz <oli.junc@gmail.com>
2024-08-02 05:17:31 +0200

    Made necessary changes in TaskApp.kt for preferences to work

--- app/src/main/java/com/junclabs/planner/TaskApp.kt
+++ app/src/main/java/com/junclabs/planner/TaskApp.kt
@@ -2,13 +2,25 @@ package com.junclabs.planner
 
 import android.app.Application
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.junclabs.planner.data.UserPreferencesRepository
 import dagger.hilt.android.HiltAndroidApp
+
+private const val MODE_PREFERENCE_NAME = "mode_preferences"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = MODE_PREFERENCE_NAME
+)
 
 @HiltAndroidApp
 class TaskApp : Application() {
+    lateinit var userPreferencesRepository: UserPreferencesRepository
+
     override fun onCreate() {
         super.onCreate()
         instance = this
+        userPreferencesRepository = UserPreferencesRepository(dataStore)
     }
 
     val context: Context
